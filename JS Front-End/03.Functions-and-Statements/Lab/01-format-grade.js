3a706a4ec8551424caa2bfec8e29d08ba0800538
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-11-06 01:26:21 +0200

    Added Lab folder with task 01 solution

---
+++ JS Front-End/03.Functions-and-Statements/Lab/01-format-grade.js
@@ -0,0 +1,21 @@
+function formatGrade(grade) {
+    let result;
+
+    if (grade < 3.00) {
+        result = 'Fail (2)';
+    } else if (grade < 3.5) {
+        result = `Poor (${grade.toFixed(2)})`;
+    } else if (grade < 4.5) {
+        result = `Good (${grade.toFixed(2)})`;
+    } else if (grade < 5.5) {
+        result = `Very good (${grade.toFixed(2)})`;
+    } else {
+        result = `Excellent (${grade.toFixed(2)})`;
+    }
+
+    console.log(result);
+}
+
+formatGrade(3.33);
+formatGrade(4.50);
+formatGrade(2.99);
