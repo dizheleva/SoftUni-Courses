8e62a833c13c9bc3a31757a6e64d4f298644a788
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-12-12 17:48:20 +0200

    Updated solutions

--- JS Front-End/06.DOM-Events/06.DOM-Events-Exercise/09. JavaScript-Quiz/quiz.js
+++ JS Front-End/06.DOM-Events/06.DOM-Events-Exercise/09. JavaScript-Quiz/quiz.js
@@ -1,22 +1,13 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    let points = 0;
-
-    let currSection = 0;
-    const questions = Array.from(document.getElementsByTagName('section'));
     const correctAnswers = ["onclick", "JSON.stringify()", "A programming API for HTML and XML documents"];
+
+    let points = 0;
+    const questions = Array.from(document.getElementsByTagName('section'));
 
     for (let i = 0; i < questions.length; i++) {
         const question = questions[i];
-
-        if (question.hasClassName('hidden') == true) {
-            question.style.display = 'none';
-        } else {
-            if (i != question.length - 1) {
-                questions[i + 1].style.display = 'block';
-            }
-        }
         const answers = Array.from(question.querySelectorAll('.quiz-answer'));
 
         answers.forEach(answer => answer.addEventListener('click', (e) => {
@@ -25,25 +16,35 @@ function solve() {
             };
 
             question.classList.add('hidden');
-            if (question.hasClassName('hidden') == true) {
-                question.style.display = 'none';
+
+            if (i < questions.length - 1) {
+                questions[i + 1].classList.remove('hidden');
             } else {
-                if (i != question.length - 1) {
-                    questions[i + 1].style.display = 'block';
-                }
+                displayResult(points);
             }
         }));
-
-        
     }
 
-    if (questions.includes(question => question.hasClassName('hidden') == false)) {
-        document.querySelector('#results').style.display = 'block';
+    function displayResult(points) {
         let text = '';
-        points === 3
-            ? text = 'You are recognized as top JavaScript fan!'
-            : text = `You have ${points} right answers`;
-        document.querySelector('#results').textContent = text;
-    }
+        switch (points) {
+            case questions.length:
+                text = 'You are recognized as top JavaScript fan!';
+                break;
 
+            case 1:
+                text = `You have ${points} right answer`;
+                break;
+
+            default:
+                text = `You have ${points} right answers`;
+                break;
+        }
+
+        const resultDiv = document.querySelector('#results');
+        const resultH1 = document.createElement('h1');
+        resultH1.textContent = text;
+        resultDiv.appendChild(resultH1);
+        resultDiv.style.display = 'block';
+    }
 }
