0633390a0bb9f157e7d0e8943ba83362841c33b1
Dilyana Zheleva <di.zheleva89@gmail.com>
2025-06-30 00:55:10 +0300

    added Watchlist service

---
+++ C# WEB/ASP.NET Introduction/ASP.NET Core Introduction - Exercises/CinemaWeb-May-2025/CinemaApp.Services.Core/Interfaces/IWatchlistService.cs
@@ -0,0 +1,9 @@
+﻿namespace CinemaApp.Services.Core.Interfaces
+{
+    using CinemaApp.Web.ViewModels.Watchlist;
+
+    public interface IWatchlistService
+    {
+        Task<IEnumerable<WatchlistViewModel>> GetUserWatchlistAsync(string userId);
+    }
+}
---
+++ C# WEB/ASP.NET Introduction/ASP.NET Core Introduction - Exercises/CinemaWeb-May-2025/CinemaApp.Services.Core/Services/WatchlistService.cs
@@ -0,0 +1,20 @@
+﻿namespace CinemaApp.Services.Core.Services
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using CinemaApp.Data;
+    using CinemaApp.Services.Core.Interfaces;
+    using CinemaApp.Web.ViewModels.Watchlist;
+
+    public class WatchlistService : IWatchlistService
+    {
+        private readonly ApplicationDbContext _context; 
+
+        public WatchlistService(ApplicationDbContext context)
+        {
+            _context = context;
+        }   
+
+        public Task<IEnumerable<WatchlistViewModel>> GetUserWatchlistAsync(string userId) => throw new NotImplementedException();
+    }
+}
