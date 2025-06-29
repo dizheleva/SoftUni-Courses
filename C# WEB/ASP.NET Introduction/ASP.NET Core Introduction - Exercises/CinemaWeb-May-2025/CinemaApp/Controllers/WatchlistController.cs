cc1bc69198a8640751f4b8d0e7c81e3a9787acd5
Dilyana Zheleva <di.zheleva89@gmail.com>
2025-06-30 00:48:13 +0300

    Implemented Watchlist View

---
+++ C# WEB/ASP.NET Introduction/ASP.NET Core Introduction - Exercises/CinemaWeb-May-2025/CinemaApp/Controllers/WatchlistController.cs
@@ -0,0 +1,39 @@
+﻿namespace CinemaApp.Web.Controllers
+{
+    using CinemaApp.Web.ViewModels.Watchlist;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class WatchlistController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new List<WatchlistViewModel>());
+        }
+
+        //Add a movie to the watchlist
+        [HttpPost]
+        public IActionResult Add(WatchlistViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Logic to add the movie to the watchlist
+                // This could involve saving to a database or session
+                return RedirectToAction("Index");
+            }
+            return View("Index", model);
+        }
+
+        //Remove a movie from the watchlist
+        [HttpPost]
+        public IActionResult Remove(string movieId)
+        {
+            if (!string.IsNullOrEmpty(movieId))
+            {
+                // Logic to remove the movie from the watchlist
+                // This could involve removing from a database or session
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
